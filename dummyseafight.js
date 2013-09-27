ef96dec90bf7cf302d9c5359399d63213542f7aa
mickael <mickael.metesreau@gmail.com>
2013-09-27 23:35:23 +0200

    Engine can find and destroy boats

--- dummyseafight.js
+++ dummyseafight.js
@@ -36,19 +36,62 @@ var play = function(game,referee,moveId,tray,turn) {
 	var value = "H5C11H4D71V3H21V3B71V2I61";
 
 	if (turn != "1") {
-		do {
-
-			var col = Math.floor((Math.random()*10));
-			var row = Math.floor((Math.random()*10));
+	
+		if (tray != '' && tray.indexOf("2") != -1) {
+			var indexNextShoot = tryToDestroyBoat(tray.indexOf("2"), tray);
+				
+			var col = indexNextShoot%10;
+			var row = (indexNextShoot - col)/10;
 
 			value = nameColumns[col] + row.toString();
+		}
+		else {
+		
+			do {
+			
+				var row = Math.floor((Math.random()*10));
+				var col = Math.floor((Math.random()*10));
+				
+				value = nameColumns[col] + row.toString();
 
-		} while(tray != '' && tray[(row * 10)+ col] != "0");
+			} while(tray != '' && tray[(row * 10)+ col] != "0");
+		}
 	}
 
 	sendResponseToReferee(referee,game,moveId,value);
 };
 
+var tryToDestroyBoat = function(indexShoot,tray) {
+	var indexNextShoot = '';
+	
+	var indexIsValid = function(index) {
+		return index >= 0 && index < 100;
+	};
+	
+	var top = indexShoot - 10, left = indexShoot - 1, bottom = indexShoot + 10, right = indexShoot + 1;
+	
+	var delta = (((tray[left] == "1" || !indexIsValid(left))  && (tray[right] == "1" || !indexIsValid(right))) || (indexIsValid(bottom) && tray[bottom] == "2") || (indexIsValid(top) && tray[top] == "2")) ? 10 : 1;
+
+	var tmp = indexShoot;
+	
+	do {
+		tmp += delta;
+
+		if (indexIsValid(tmp)) {
+			if (tray[tmp] == "0") 
+				indexNextShoot = tmp;
+			else if (tray[tmp] != "2")
+				delta = -delta;
+		}
+		else {
+			delta = -delta;
+		}
+			
+	} while (indexNextShoot == '');
+	
+	return indexNextShoot;
+};
+		
 var server = http.createServer(function(request, response) {
 
 	if (request.method == "GET") {
@@ -66,5 +109,3 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen(conf.port);
-
-
